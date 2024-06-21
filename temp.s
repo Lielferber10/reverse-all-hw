33ab53b07e5aadb686750f0733958e2bc6fb096e
lielFerber <102861720+Lielferber10@users.noreply.github.com>
2024-06-21 18:39:29 +0300

    add changes

--- temp.s
+++ temp.s
@@ -1,20 +1,71 @@
 .intel_syntax noprefix
 
-.LC0:
-        .string "%s"
-.LC1:
-        .string "%02d\n"
-.LC2:
-        .string "Wood"
-.LC3:
-        .string "Brick"
-.LC4:
-        .string "Wool"
-.LC5:
-        .string "Grain"
-.LC6:
-        .string "Stone"
 main:
+        push    ebp                     # store ebp
+        mov     ebp, esp
+
+        push    0x0       		# pushing null
+        push    0x41797261		# pushing A,y,r,a
+        push    0x7262694c		# pushing r,b,i,L
+        push    0x64616f4c		# pushing d,a,o,L
+        push    esp                     # push pointer for "LoadLibraryA"
+
+        call    FindFunction            # call FindFunction("LoadLibraryA")
+        add     esp, 0x14               # clear stack
+
+        push    0x00006c6c		# pushing null,l,l
+        push    0x642e7472		# pushing d,#,t,r
+        push    0x6376736d		# pushing c,v,s,m
+        push    esp
+
+        call    eax                     # call LoadLibrary("msvcrt.dll")
+        add     esp, 0x0c               # clear stack (note arguments are cleared already)
+
+        push    eax                     # store module handle for msvcrt
+        push    0x00007373		# pushing null,s,s
+        push    0x65726464		# pushing e,r,d,d
+        push    0x41636f72		# pushing A,c,o,r
+        push    0x50746547		# pushing P,t,e,G
+        push    esp                     # push pointer for "GetProcAddress"
+
+        call    FindFunction            # call FindFunction("GetProcAddress")
+        add     esp, 0x14               # clear stack
+        pop     ebx                     # restore module handle for msvcrt
+
+        push    0x00006674		# pushing null,f,t
+        push    0x6e697270		# pushing n,i,r,p
+        push    esp                     # push pointer for "printf"
+        push    ebx                     # push module handle for msvcrt
+
+        call    eax                     # call GetProcAddress(msvcrt, "printf")
+        add     esp, 0x08               # clear stack (note arguments are cleared already)
+
+        mov     esi, eax
+        mov     eax, ebx
+        push    eax                     # store module handle for msvcrt
+        push    0x00007373		# pushing null,s,s
+        push    0x65726464		# pushing e,r,d,d
+        push    0x41636f72		# pushing A,c,o,r
+        push    0x50746547		# pushing P,t,e,G
+        push    esp                     # push pointer for "GetProcAddress"
+
+        call    FindFunction            # call FindFunction("GetProcAddress")
+        add     esp, 0x14               # clear stack
+        pop     ebx                     # restore module handle for msvcrt
+
+        push    0x00000066              # pushing null,f
+        push    0x6e616373              # pushing n,a,c,s
+        push    esp                     # push pointer for "scanf"
+        push    ebx                     # push module handle for msvcrt
+
+        call    eax                     # call GetProcAddress(msvcrt, "scanf")
+        add     esp, 0x08               # clear stack (note arguments are cleared already)
+        mov     ebx, eax
+
+
+        #from now on, ebx = &scanf, esi = &printf
+
+
         lea     ecx, [esp+4]
         and     esp, -16
         push    DWORD PTR [ecx-4]
@@ -31,9 +82,10 @@ main:
         sub     esp, 8
         lea     eax, [ebp-44]
         push    eax
-        push    OFFSET FLAT:.LC0
-        call    __isoc99_scanf
-        add     esp, 16
+        push    0x007325		# pushing null,s,%
+        push    esp                     # push pointer for "%s"
+        call    ebx                     # call scanf(%s, input)
+        add     esp, 20                 # clear stack
         cmp     eax, -1
         setne   al
         test    al, al
@@ -94,55 +146,77 @@ main:
         mov     eax, DWORD PTR [ebp-28]
         sub     esp, 8
         push    eax
-        push    OFFSET FLAT:.LC1
-        call    printf
-        add     esp, 16
+        push    0x0000000A              # pushing null,\n
+        push    0x64323025              # pushing d,2,0,%
+        push    esp                     # push pointer for "%02d\n"
+        call    esi                     # call printf("%02d\n", resources[0])
+        add     esp, 24                 # clear stack
         sub     esp, 12
-        push    OFFSET FLAT:.LC2
-        call    puts
-        add     esp, 16
+        push    0x00                    # pushing null
+        push    0x646F6F57              # pushing d,o,o,W
+        push    esp                     # push pointer for "Wood"
+        call    esi                     # call printf("Wood")
+        add     esp, 24                 # clear stack
         mov     eax, DWORD PTR [ebp-24]
         sub     esp, 8
         push    eax
-        push    OFFSET FLAT:.LC1
-        call    printf
-        add     esp, 16
+        push    0x0000000A              # pushing null,\n
+        push    0x64323025              # pushing d,2,0,%
+        push    esp                     # push pointer for "%02d\n"
+        call    esi                     # call printf("%02d\n", resources[1])
+        add     esp, 24                 # clear stack
         sub     esp, 12
-        push    OFFSET FLAT:.LC3
-        call    puts
-        add     esp, 16
+        push    0x0000006B              # pushing null, k
+        push    0x63697242              # pushing c,i,r,B
+        push    esp                     # push pointer for "Brick"
+        call    esi                     # call printf("Brick")
+        add     esp, 24                 # clear stack
         mov     eax, DWORD PTR [ebp-20]
         sub     esp, 8
         push    eax
-        push    OFFSET FLAT:.LC1
-        call    printf
-        add     esp, 16
+        push    0x0000000A              # pushing null,\n
+        push    0x64323025              # pushing d,2,0,%
+        push    esp                     # push pointer for "%02d\n"
+        call    esi                     # call printf("%02d\n", resources[2])
+        add     esp, 24                 # clear stack
         sub     esp, 12
-        push    OFFSET FLAT:.LC4
-        call    puts
-        add     esp, 16
+        push    0x00000000              # pushing null
+        push    0x6c6f6f57              # pushing l,o,o,W 
+        push    esp                     # push pointer for "Wool"
+        call    esi                     # call printf("Wool")
+        add     esp, 24                 # clear stack
         mov     eax, DWORD PTR [ebp-16]
         sub     esp, 8
         push    eax
-        push    OFFSET FLAT:.LC1
-        call    printf
-        add     esp, 16
+        push    0x0000000A              # pushing null,\n
+        push    0x64323025              # pushing d,2,0,%
+        push    esp                     # push pointer for "%02d\n"
+        call    esi                     # call printf("%02d\n", resources[3])
+        add     esp, 24                 # clear stack
         sub     esp, 12
-        push    OFFSET FLAT:.LC5
-        call    puts
-        add     esp, 16
+        push    0x0000006e              # pushing null,n
+        push    0x69617247              # pushing i,a,r,G
+        push    esp                     # push pointer for "Grain"
+        call    esi                     # call printf("Grain")
+        add     esp, 24                 # clear stack
         mov     eax, DWORD PTR [ebp-12]
         sub     esp, 8
         push    eax
-        push    OFFSET FLAT:.LC1
-        call    printf
-        add     esp, 16
+        push    0x0000000A              # pushing null,\n
+        push    0x64323025              # pushing d,2,0,%
+        push    esp                     # push pointer for "%02d\n"
+        call    esi                     # call printf("%02d\n", resources[4])
+        add     esp, 24                 # clear stack
         sub     esp, 12
-        push    OFFSET FLAT:.LC6
-        call    puts
-        add     esp, 16
+        push    0x00000065              # pushing null,e
+        push    0x6e6f7453              # pushing n,o,t,S
+        push    esp                     # push pointer for "Stone"
+        call    esi                     # call printf("Stone")
+        add     esp, 24                 # clear stack
         mov     eax, 0
         mov     ecx, DWORD PTR [ebp-4]
         leave
         lea     esp, [ecx-4]
         ret
+
+FindFunction:                           # placeholder for search_kernel32
