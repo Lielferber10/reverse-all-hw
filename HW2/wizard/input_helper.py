0315345f4ca23369f21e30c4ca7ade87d608831d
orel-david <42713127+orel-david@users.noreply.github.com>
2024-07-08 11:35:04 +0300

    PROG IN WIZ

---
+++ HW2/wizard/input_helper.py
@@ -0,0 +1,15 @@
+expected = 'CEFKOIAMMBLAICBDEMDEBNGNFBCK'
+expected = expected[::-1]
+res = 'I'
+
+for i in range(1,len(expected)):
+    c1 = ord(res[-1]) - 0x41
+    c2 = ord(expected[i]) - 0x41
+    res += str(chr((c1^c2)+0x41))
+
+res = res[::-1]
+
+for i in range(16):
+    res += str('0')
+
+print(res)
